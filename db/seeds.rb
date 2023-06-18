283d7912d3bf4b62c2118df44455f307bf4ecc9f
Steven <digitalexile99@gmail.com>
2023-06-17 22:40:53 -0600

    fixed build script

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,13 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 # Create users
-user1 = User.create(name: "John Doe", email: "john@example.com", password: "password")
-user2 = User.create(name: "Jane Smith", email: "jane@example.com", password: "password")
+# user1 = User.create(name: "John Doe", email: "john@example.com", password: "password")
+# user2 = User.create(name: "Jane Smith", email: "jane@example.com", password: "password")
 
-# Create services
-service1 = Service.create(name: "Classic Eyelash Extensions", description: "Adds length and volume to natural lashes", price: 50.00)
-service2 = Service.create(name: "Volume Eyelash Extensions", description: "Creates a fuller and more dramatic look", price: 80.00)
+# # Create services
+# service1 = Service.create(name: "Classic Eyelash Extensions", description: "Adds length and volume to natural lashes", price: 50.00)
+# service2 = Service.create(name: "Volume Eyelash Extensions", description: "Creates a fuller and more dramatic look", price: 80.00)
 
-# Create appointments
-appointment1 = Appointment.create(user: user1, service: service1, start_time: DateTime.now + 1.day)
-appointment2 = Appointment.create(user: user2, service: service2, start_time: DateTime.now + 2.days)
+# # Create appointments
+# appointment1 = Appointment.create(user: user1, service: service1, start_time: DateTime.now + 1.day)
+# appointment2 = Appointment.create(user: user2, service: service2, start_time: DateTime.now + 2.days)
