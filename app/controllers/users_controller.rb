7dc8f39c147dfb61e881e4c1639055ed6ac7c932
Steven <digitalexile99@gmail.com>
2023-06-17 21:36:45 -0600

    added route handlers to services

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,41 +2,40 @@ class UsersController < ApplicationController
     def index
         users = User.all
         render json: users
-      end
+    end
     
-      def show
+    def show
         user = User.find(params[:id])
         render json: user
-      end
+    end
     
-      def create
+    def create
         user = User.new(user_params)
         if user.save
           render json: user
         else
           render json: { error: user.errors.full_messages }, status: :unprocessable_entity
         end
-      end
+    end
     
-      def update
+    def update
         user = User.find(params[:id])
         if user.update(user_params)
           render json: user
         else
           render json: { error: user.errors.full_messages }, status: :unprocessable_entity
         end
-      end
+    end
     
-      def destroy
+    def destroy
         user = User.find(params[:id])
         user.destroy
         render json: { message: "User deleted successfully" }
-      end
+    end
     
-      private
+    private
     
-      def user_params
+    def user_params
         params.require(:user).permit(:name, :email, :password)
-      end
-    
+    end 
 end
